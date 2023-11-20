210d15e6f0a6fccd524d127a39fbf09ac99a14e6
Ky Phan <kyphanz1@gmail.com>
2023-11-21 00:11:53 +0700

    .

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+import {
+  Entypo,
+  MaterialIcons,
+  Ionicons,
+  FontAwesome
+} from '@expo/vector-icons';
+
+import Color from '../../constants/Color';
+
+const Layout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Color.primary
+      }}>
+      <Tabs.Screen
+        name='index'
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name='home' color={color} size={size} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='explore'
+        options={{
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name='explore' color={color} size={size} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='reserved'
+        options={{
+          tabBarLabel: 'Reserved',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='md-fast-food' size={size} color={color} />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name='profile'
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name='user' size={size} color={color} />
+          )
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default Layout;
